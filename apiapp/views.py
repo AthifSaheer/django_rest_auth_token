b9f573e1b33d052e11e9b5dfa34417521935a1b1
Athif Saheer <liteboook@gmail.com>
2021-06-26 15:55:12 +0530

    some features added

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserRegister
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-# Create your views here.
+class registration(APIView):
+    def post(self, request, format=None):
+        serializer = UserRegister(data = request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = 'registered'
+            data['username'] = account.username
+            data['email'] = account.email
+            token, create = Token.objects.get_or_create(user=account).key
+            data['token'] = token.key
+        else:
+            data = serializer.errors
+        return Response(data)
